2ad77fda6593af2f3d7589ab71af9f85db93c203
Franco Romeo <francoromeo011@hotmail.com>
2021-09-23 13:15:30 -0300

    Primer commit

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="entry-container mb-3 pointer p-2" 
+         @click="$router.push({ name: 'entry', params: { id: 10 } })">
+        <!-- Titulo -->
+        <div class="entry-title d-flex">
+            <span class="text-success fs-5 fw-bold">22</span>
+            <span class="mx-1 fs-5">Septiembre</span>
+            <span class="mx-1 fw-light">2021, miercoles</span>
+        </div>
+
+        <div class="entry-description">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore voluptas aliquam unde nihil! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos esse soluta atque aliquid at?
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.entry-container{
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+
+    &:hover{
+        background-color: lighten($color: grey, $amount: 45);
+        transition: 0.2s all ease-in;
+    }
+    .entry-description{
+        font-size: 12px;
+    }
+}
+</style>
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toTimeString(),
+            text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores voluptatum, porro, sapiente placeat necessitatibus quod deleniti quisquam enim obcaecati nostrum nihil, reprehenderit consequatur ducimus? Accusantium explicabo ipsum laborum modi? Voluptas aperiam omnis sit? Autem doloremque officia, ea quas at reprehenderit eos fuga odio, perferendis quisquam quo. Perspiciatis dolor fuga saepe.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 7865,
+            date: new Date().toTimeString(),
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium aperiam accusamus exercitationem! Pariatur qui exercitationem aliquam reprehenderit laboriosam atque, similique accusantium dolorem ea sapiente eligendi fugiat maxime dignissimos debitis mollitia quae commodi, repellat, omnis consectetur dolorum quo. Voluptate tenetur, nostrum dignissimos enim excepturi illo blanditiis recusandae atque molestias officia exercitationem.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 3565,
+            date: new Date().toTimeString(),
+            text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, natus laudantium? Eligendi dolor mollitia hic odio obcaecati id. Incidunt aliquid, ullam quaerat repellat nulla magnam quo facere dolor! Illum officiis culpa recusandae sequi? Vitae, ducimus culpa iste eos amet commodi labore adipisci minima!',
+            picture: null,
+        },
+    ]
+})
